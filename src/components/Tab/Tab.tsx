b62562127cc3776769a600698782a5a8e59984c9
cheived <cheived55@gmail.com>
2024-07-09 08:48:33 +0400

    tab indent fix

--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -16,9 +16,9 @@ export default function Tab({ data }: { data: Data }) {
     return (
         <nav className="flex">
             {data ? data.map((item, i) => {
-                let classList = "px-4 py-2.5 text-button text-text-primary flex gap-2 items-center text-button uppercase"
+                let classList = "px-4 py-2.5 text-button flex gap-2 items-center text-button uppercase relative"
                 if (active == i) {
-                    classList += " border-border-accent border-b-2 text-text-primary"
+                    classList += " text-text-primary"
                 } else {
                     classList += " text-text-secondary"
                 }
@@ -27,6 +27,7 @@ export default function Tab({ data }: { data: Data }) {
                         {item.title}
                         {item.count ? <div className="px-2 py-0.5 rounded-3xl bg-bg-accent/50" >{item.count}</div> : null}
                         {item.childrens.length ? <button className="flex items-center"><Image src="/img/chevron-down.svg" alt="chevron-down" className="h-4" width={16} height={16} /></button> : null}
+                        {active == i ? <div className="absolute w-full bg-border-accent bottom-0 left-0 h-[2px]"></div> : null}
                     </button>
                 )
             }) : null}
